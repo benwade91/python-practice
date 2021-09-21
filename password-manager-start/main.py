5531516eb9aac6bc763d0f482b4ac2a0958401a7
benwade91 <70988204+benn925@users.noreply.github.com>
2021-09-21 10:53:30 -0700

    added input validation and writing to file for saved info

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,9 +1,44 @@
 from tkinter import *
+from tkinter import messagebox
+import re
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save_password():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    if len(website) == 0:
+        messagebox.showerror(title='Invalid Website', message='Please enter a website name')
+
+    elif not validate_email(email):
+        messagebox.showerror(title='Invalid Email', message='Please enter a valid email')
+
+    elif len(password) == 0:
+        messagebox.showerror(title='Invalid Password', message='Please enter a password')
+
+    else:
+        confirm_info = messagebox.askokcancel(title=website, message=f"email: {email} \n "
+                                                                     f"password: {password} \n is this correct?")
+        if confirm_info:
+            pw_file = open('passwords.txt', 'a')
+            pw_file.write(f"{website} | {email} | {password} \n")
+            pw_file.close()
+            website_input.delete(0, END)
+            email_input.delete(0, END)
+            password_input.delete(0, END)
+
+
+def validate_email(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -39,7 +74,7 @@ password_input.grid(column=1, row=3)
 generate_btn = Button(text='Generate Password')
 generate_btn.grid(column=2, row=3)
 
-add_btn = Button(text='Add', width=36)
+add_btn = Button(text='Add', width=36, command=save_password)
 add_btn.grid(column=1, columnspan=2, row=4)
 
 window.mainloop()

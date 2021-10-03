eec6fa378330ff9acfb3ec087cd4c76379f31c0b
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-03 06:44:06 -0700

    UI now responds with red or green background according to the correctness of user input

--- API-Quiz/ui.py
+++ API-Quiz/ui.py
@@ -1,29 +1,57 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz: QuizBrain):
+        self.quiz = quiz
         self.window = Tk()
         self.window.title('Quiz Game')
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
 
         self.score_label = Label(text="Score: 0", background=THEME_COLOR, foreground="white")
-        self.score_label.grid(row=0, column=1, padx=20, pady=20,)
+        self.score_label.grid(row=0, column=1, padx=20, pady=20, )
 
         self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 207, text="Ready?",
-                                                     width=250, font=("Arial", 20, "italic"), fill="white",
+        self.question_text = self.canvas.create_text(150, 125, text="Ready?",
+                                                     width=280, font=("Arial", 20, "italic"), fill="black",
                                                      tag='question_text')
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
 
         self.true_img = PhotoImage(file='images/true.png')
-        self.true_btn = Button(image=self.true_img, highlightthickness=0)
-        self.true_btn.grid(row=2, column=0, padx=20, pady=20,)
+        self.true_btn = Button(image=self.true_img, highlightthickness=0, command=self.true_answer)
+        self.true_btn.grid(row=2, column=0, padx=20, pady=20, )
 
         self.false_img = PhotoImage(file='images/false.png')
-        self.false_btn = Button(image=self.false_img, highlightthickness=0)
-        self.false_btn.grid(row=2, column=1, padx=20, pady=20,)
+        self.false_btn = Button(image=self.false_img, highlightthickness=0, command=self.false_answer)
+        self.false_btn.grid(row=2, column=1, padx=20, pady=20, )
+
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg='white')
+        question = self.quiz.next_question()
+        print(question)
+        self.canvas.itemconfig(self.question_text, text=question)
+
+    def true_answer(self):
+        answer = self.quiz.check_answer("True")
+        self.ui_feedback(answer)
+
+    def false_answer(self):
+        answer = self.quiz.check_answer("False")
+        self.ui_feedback(answer)
+
+    def ui_feedback(self, answer):
+        if answer:
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            self.canvas.config(bg='green')
+            self.window.after(1000, self.get_next_question)
+        else:
+            self.canvas.config(bg='red')
+            self.window.after(1000, self.get_next_question)
+

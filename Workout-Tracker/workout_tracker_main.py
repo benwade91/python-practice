0b2fe1336197da196469212c4ef03584a40ce3a6
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-07 15:16:28 -0700

    added sheety API for writing to google doc from nutritionix response

--- Workout-Tracker/workout_tracker_main.py
+++ Workout-Tracker/workout_tracker_main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 
 nutritionix_ep = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
@@ -7,13 +8,32 @@ nutritionix_header = {
     'x-app-id': os.environ.get('NUTRITION-X-APP-ID'),
     'x-app-key': os.environ.get('NUTRITION-API-KEY')
 }
+
+workout = input("What was your workout today?")
+
 user_info = {
- "query":"ran 3 miles",
- "gender":"female",
- "weight_kg":72.5,
- "height_cm":167.64,
- "age":30
+ "query": workout,
+ "gender": "Male",
+ "weight_kg": 72.5,
+ "height_cm": 167.64,
+ "age": 30
 }
 
 response = requests.post(url=nutritionix_ep, json=user_info, headers=nutritionix_header)
 print(response.text)
+
+for workout in response.json()['exercises']:
+    google_url = 'https://api.sheety.co/bed2391b4b626481b6b70abde42aa10d/myWorkouts/workouts'
+    google_json = {
+        'workout': {
+            'date': f"{datetime.now().strftime('%d/%m/%Y')}",
+            'time': f"{datetime.now().strftime('%X')}",
+            'exercise': f"{workout['name'].title()}",
+            'duration': f"{workout['duration_min']}",
+            'calories': f"{workout['nf_calories']}"
+    }}
+    google_response = requests.post(url=google_url, json=google_json)
+    print(google_response.json())
+
+new_response = requests.get(url='')
+print(new_response.json())

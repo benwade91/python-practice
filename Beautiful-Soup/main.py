d5877c44de75decc6f0aeffced55e7851279dc57
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-12 07:17:25 -0700

    script scrapes movie data for top 100 movies and writes them to list in txt file

--- Beautiful-Soup/main.py
+++ Beautiful-Soup/main.py
@@ -1,8 +1,21 @@
+import re
 from bs4 import BeautifulSoup
+import requests
 
-with open('website.html') as html:
-    contents = html.read()
+response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+movie_page = response.text
 
-soup = BeautifulSoup(contents, 'html.parser')
+soup = BeautifulSoup(movie_page, 'html.parser')
 
-print(soup.title.name)
+
+gallery = soup.select_one(selector=".listicle-container")
+all_images = gallery.select(selector=".image-container img")
+all_movies = []
+for image in all_images:
+    all_movies.append(image.get("alt"))
+all_movies.reverse()
+
+
+with open('movie_list.txt', 'w') as movie_list:
+    for index, movie in enumerate(all_movies):
+        movie_list.write(f"{index + 1}) {movie} \n")

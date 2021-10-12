fa6497f5021565c3675b7d89f326b5744e6148c9
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-12 13:41:19 -0700

    moved header info to env

--- Amazon-Price-Bot/main.py
+++ Amazon-Price-Bot/main.py
@@ -5,12 +5,16 @@ import smtplib
 
 url = "https://www.amazon.com/dp/B08C4LC7TG/ref=cm_sw_em_r_mt_dp_EK6PZQ3189HGD23KWPBR"
 header = {
-
+    "Accept-Encoding":"gzip, deflate",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "X-Forwarded-For": os.environ.get('IP'),
+    "Accept-Language": "en-US,en;q=0.9",
 }
 r = requests.get(url, headers=header).text
 soup = BeautifulSoup(r, 'html.parser')
 
 price = float(soup.find(id="priceblock_ourprice").text.replace("$", ""))
+print(price)
 
 if price < 420:
     with smtplib.SMTP('smtp.mail.yahoo.com', 587) as connection:

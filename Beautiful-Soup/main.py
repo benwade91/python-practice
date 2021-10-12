e6d0810e317a3dbf9b3dd7d95b4b453fbfeddad3
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-12 08:08:49 -0700

    updated methods for gathering movie titles. both are currently incomplete

--- Beautiful-Soup/main.py
+++ Beautiful-Soup/main.py
@@ -4,18 +4,24 @@ import requests
 
 response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
 movie_page = response.text
-
 soup = BeautifulSoup(movie_page, 'html.parser')
 
+# ---------SPLITS TITLE FROM REVIEW LINK-------------#
+# THIS METHOD LEAVES OUT THREE MOVIES THAT ARE MISSING REVIEWS
+data = soup.find_all("a", text=re.compile("Read Empire's review of "))
+movie_titles = [string.text.split("Read Empire's review of ")[1] for string in data]
+movie_titles.reverse()
 
+# ---------GETS TITLE DATA FROM IMG ALT TAGS--------#
+# THIS METHOD GATHERS A FEW INCORRECT TITLES FROM THE ALT TAGS
 gallery = soup.select_one(selector=".listicle-container")
-all_images = gallery.select(selector=".image-container img")
-all_movies = []
-for image in all_images:
-    all_movies.append(image.get("alt"))
-all_movies.reverse()
+images = gallery.select(selector=".image-container img")
+movies = []
+for image in images:
+    movies.append(image.get("alt"))
+movies.reverse()
 
 
 with open('movie_list.txt', 'w') as movie_list:
-    for index, movie in enumerate(all_movies):
+    for index, movie in enumerate(movies):
         movie_list.write(f"{index + 1}) {movie} \n")

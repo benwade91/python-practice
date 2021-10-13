696b80f4a1fedffacc042c1110caf5500fd60206
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-13 13:57:14 -0700

    initial connection to selenium endpoint

--- Selenium-Practice/main.py
+++ Selenium-Practice/main.py
@@ -1,2 +1,7 @@
+from selenium import webdriver
 
-chrome = '/Users/benjaminwade/Desktop/python-practice'
+chrome_driver_path = '/Users/benjaminwade/Desktop/python-practice/chromedriver'
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get('https://amazon.com')

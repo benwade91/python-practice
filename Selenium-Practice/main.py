ffb9f741b329b3f8f084343fa91f6e4e6167a91e
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-14 11:55:10 -0700

    program scrapes event data from python.org and creates a dictionary of events

--- Selenium-Practice/main.py
+++ Selenium-Practice/main.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = '/Users/benjaminwade/Desktop/python-practice/chromedriver'
+s = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=s)
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get('https://www.python.org/')
 
-driver.get('https://amazon.com')
+event_list = driver.find_element(By.CLASS_NAME, 'event-widget')
+line_items = event_list.find_elements(By.TAG_NAME, 'li')
+
+event_dic = {}
+
+for index, item in enumerate(line_items):
+    time = item.find_element(By.TAG_NAME, 'time')
+    location = item.find_element(By.TAG_NAME, 'a')
+    event_dic[index] = {
+            'time': time.text,
+            'name': location.text
+        }
+print(event_dic)
+driver.quit()

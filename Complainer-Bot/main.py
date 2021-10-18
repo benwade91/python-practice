4e973fb99d86473bab9a3dbaa0c02d6e6d79bb58
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-18 12:02:55 -0700

    uncommented speed test portion of script

--- Complainer-Bot/main.py
+++ Complainer-Bot/main.py
@@ -11,19 +11,20 @@ s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 driver.implicitly_wait(10)
 
-# driver.get('https://www.speedtest.net/')
+driver.get('https://www.speedtest.net/')
 driver.maximize_window()
 
 # --------START WEB SPEED ASSESSMENT
-# driver.find_element(By.CLASS_NAME, 'start-text').click()
-#
-# time.sleep(0)
-#
-# dwnld_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
-# upld_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
-# print(dwnld_speed.text)
-# print(upld_speed.text)
+driver.find_element(By.CLASS_NAME, 'start-text').click()
 
+time.sleep(0)
+
+dwnld_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+upld_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
+print(dwnld_speed.text)
+print(upld_speed.text)
+
+# ------CREATE TWITTER BOT AND SEND TWEET.
 twitter_bot = TwitterBot(os.environ.get("TWITTER_EMAIL"), os.environ.get("TWITTER_PW"), driver)
 twitter_bot.login()
 time.sleep(3)

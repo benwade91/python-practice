5d50b7aca7faffd10fb2565d6beb85566b3fd910
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-23 07:05:25 -0700

    html template for making api calls to mildly offensive age/gender guesses

--- Jinja-Blog/main.py
+++ Jinja-Blog/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,13 @@ def home_route():
     year = datetime.now().year
     return render_template('index.html', year=year)
 
+@app.route('/guess/<name>')
+def guess(name):
+    gender_api = requests.get(f"https://api.genderize.io?name={name}")
+    gender = gender_api.json()['gender']
+    age_api = requests.get(f"https://api.agify.io?name={name}")
+    age = age_api.json()['age']
+    return render_template('guess.html', name=name, gender=gender, age=age)
 
 if __name__ == '__main__':
     app.run(debug=True)

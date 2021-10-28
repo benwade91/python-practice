9a052f80a2aface229d235769ac9824e668f0df0
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-28 12:12:18 -0700

    implemented Flask-WTF for form validation

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
 
 app = Flask(__name__)
+app.secret_key = 'a random string'
 
 
 @app.route("/")
@@ -8,9 +19,14 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = MyForm()
+    if form.validate_on_submit():
+        print(form.email.data)
+        print(form.password.data)
+        return render_template('success.html')
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)

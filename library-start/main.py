a8204a08c783a9f8fce985b6e776ea677e867fec
benwade91 <70988204+benn925@users.noreply.github.com>
2021-10-30 07:36:09 -0700

    server renders index and add book page

--- library-start/main.py
+++ library-start/main.py
@@ -7,12 +7,12 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', library=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    return render_template('add.html')
 
 
 if __name__ == "__main__":

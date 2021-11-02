1bc99e25cffad774617b01762cf6d54f81a67e54
benwade91 <70988204+benn925@users.noreply.github.com>
2021-11-02 09:42:56 -0700

    implemented SQL Alchemy to store rated books

--- library-start/main.py
+++ library-start/main.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
-all_books = []
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=False, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template('index.html', library=all_books)
 
 
@@ -21,18 +32,16 @@ def add():
     if request.method == 'POST':
         title = request.form.get('book_name', None)
         author = request.form.get('author', None)
-        rating = request.form.get('rating', None)
-        new_book = {
-            'title': title,
-            'author': author,
-            'rating': rating
-        }
-        all_books.append(new_book)
-        print(all_books)
+        rating = float(request.form.get('rating', None))
+
+        harry_p = Book(title=title, author=author, rating=rating)
+        db.session.add(harry_p)
+        db.session.commit()
+
         return redirect('/')
     return render_template('add.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 

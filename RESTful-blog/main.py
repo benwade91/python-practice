1dbafc48c59210af5b4246350774dcb3321016fc
benwade91 <70988204+benn925@users.noreply.github.com>
2021-11-08 12:26:30 -0800

    created new-post route to create new blog posts and save them to the DB

--- RESTful-blog/main.py
+++ RESTful-blog/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+import datetime
+
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -6,18 +8,18 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CONFIGURE TABLE
+
+# CONFIGURE TABLE
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -28,13 +30,13 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
@@ -44,12 +46,29 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
+@app.route("/new-post", methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'GET':
+        return render_template('make-post.html', form=CreatePostForm())
+    else:
+        date = datetime.datetime.now().strftime("%B %d, %Y")
+        title = request.form.get('title')
+        subtitle = request.form.get('subtitle')
+        author = request.form.get('author')
+        img_url = request.form.get('img_url')
+        body = request.form.get('body')
+        post = BlogPost(date=date, title=title, subtitle=subtitle, author=author, img_url=img_url, body=body)
+        db.session.add(post)
+        db.session.commit()
+        return redirect("/")
+
+
 @app.route("/post/<int:index>")
 def show_post(index):
     posts = db.session.query(BlogPost).all()
     requested_post = None
     for blog_post in posts:
-        if blog_post["id"] == index:
+        if blog_post.id == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
@@ -63,5 +82,6 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
